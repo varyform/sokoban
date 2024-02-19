e45399f5e0e70ec0b99dbafddd4761b7072c13a4
Oleh Khomei <varyform@gmail.com>
2024-02-19 18:45:32 +0200

    Tweaks

--- app/scenes/paused.rb
+++ app/scenes/paused.rb
@@ -2,7 +2,7 @@ module Scene
   class << self
     # scene reached from gameplay when the player needs a break
     def tick_paused(args)
-      draw_bg(args, DARK_YELLOW)
+      draw_bg(args, r: 30, g: 18, b: 39)
 
       options = [
         {
@@ -33,7 +33,8 @@ module Scene
         options.find { |o| o[:key] == :resume }[:on_select].call(args)
       end
 
-      args.outputs.labels << label(:paused, x: args.grid.w / 2, y: args.grid.top - 200, align: ALIGN_CENTER, size: SIZE_LG, font: FONT_BOLD)
+      # args.outputs.labels << label(:paused, x: args.grid.w / 2, y: args.grid.top - 200, align: ALIGN_CENTER, size: SIZE_LG, font: FONT_BOLD)
+      args.outputs.sprites << { x: args.grid.w / 2 - 217, y: 220.from_top, w: 434, h: 82, path: 'sprites/logo.png' }
     end
   end
 end
