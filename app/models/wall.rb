e1c8880a948b3373e2b30845d87af9f3c53f79c7
Oleh Khomei <varyform@gmail.com>
2024-02-21 11:10:03 +0200

    Update some sprites

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -4,10 +4,38 @@ class Wall < Tile
   end
 
   def to_sprite
-    super.merge(static: true)
+    super.merge!(static: true, source_x: SIZE * calculate_tile_index)
   end
 
   def weight
     20
+  end
+
+  private
+
+  def calculate_tile_index
+    @rule ||= begin
+      wall_rules.to_a.reverse.find do |rules, _i|
+        rules.all? { |direction| entity_at(direction).is_a?(Wall) }
+      end.last
+    end
+  end
+
+  def wall_rules
+    {
+      [:up]                => 5,
+      [:down]              => 10,
+      [:left]              => 11,
+      [:right]             => 2,
+      [:right, :up]        => 1,
+      [:right, :down]      => 2,
+      [:left, :right]      => 3,
+      [:left, :down]       => 4,
+      [:up, :down]         => 5,
+      [:left, :up]         => 6,
+      [:left, :up, :down]  => 7,
+      [:right, :up, :down] => 8,
+      [:left, :right, :up] => 9,
+    }
   end
 end
