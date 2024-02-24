30245228e525fb8430a65adf749912c94dbfcc68
Oleh Khomei <varyform@gmail.com>
2024-02-24 14:34:41 +0200

    Fireworks

---
+++ app/fireworks.rb
@@ -0,0 +1,28 @@
+module Fireworks
+  class << self
+    def create
+      []
+    end
+
+    def tick(fireworks, args)
+      fireworks.reject! { ParticleSystem.dead?(_1) }
+      fireworks.each { ParticleSystem.tick(args, _1) }
+      args.outputs.sprites << fireworks.flat_map(&:particles)
+    end
+
+    def launch(fireworks, args)
+      return unless args.tick_count.mod_zero?(4)
+
+      fireworks <<
+        ParticleSystem.create(
+          FireworksEffect,
+          x: rand(1200) + 40,
+          y: rand(700) + 10,
+          w: 20,
+          h: 20,
+          rate: random(30, 50),
+          duration: random(20, 50)
+        )
+    end
+  end
+end
---
+++ app/particle_system.rb
@@ -0,0 +1,52 @@
+module ParticleSystem
+  class << self
+    def create(effect, x:, y:, w: 1, h: 1, rate: 20, dir: 0, duration: nil)
+      {
+        # effect is a module that implements the Effect interface Eg: app/smoke_effect.rb
+        effect: effect,
+        rate: rate,
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        dir: dir,
+        duration: duration,
+        ticks_since_last_emission: 0,
+        particles: []
+      }
+    end
+
+    def tick(_args, sys)
+      sys.duration -= 1 if sys.duration
+
+      sys.particles.each { sys.effect.tick(_1, sys) }
+
+      return unless emit?(sys)
+
+      # rate: 5 => 12 pps; 0.2 => 300 pps. If ticks_per_emision < 1, multiple particles will be emitted in a single tick
+      ticks_per_emission = 60.fdiv(sys.rate)
+
+      # count ticks until we reach a tick where we should emit a particle.
+      sys.ticks_since_last_emission += 1
+      particles_to_emit = (sys.ticks_since_last_emission / ticks_per_emission).floor
+
+      return unless particles_to_emit.positive?
+
+      particles_to_emit.times { |_i| sys.particles << sys.effect.emit_particle(sys) }
+      sys.ticks_since_last_emission = 0
+    end
+
+    def emit?(sys)
+      true if sys.duration.nil?
+      sys.duration.positive?
+    end
+
+    def dead?(sys)
+      sys.duration <= 0 && sys.particles.empty?
+    end
+
+    def destroy(sys)
+      sys.particles = []
+    end
+  end
+end
