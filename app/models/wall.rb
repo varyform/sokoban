64fd0e4e3bd0512c9f3b0d4a9b5e7b9ad123b358
Oleh Khomei <varyform@gmail.com>
2024-02-29 11:29:50 +0200

    Do not embed wall rules into each wall

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -14,28 +14,30 @@ class Wall < Tile
   private
 
   def tile_index
-    @tile_index ||= wall_rules.to_a.reverse.find do |rules, _i|
+    @tile_index ||= self.class.wall_rules.find do |rules, _i|
       rules.all? { |direction| entity_at(direction).is_a?(Wall) }
     end.last
   end
 
-  def wall_rules
-    {
-      []                          => 10,
-      [:up]                       => 5,
-      [:down]                     => 10,
-      [:left]                     => 11,
-      [:right]                    => 2,
-      [:right, :up]               => 1,
-      [:right, :down]             => 2,
-      [:left, :right]             => 3,
-      [:left, :down]              => 4,
-      [:up, :down]                => 5,
-      [:left, :up]                => 6,
-      [:left, :up, :down]         => 7,
-      [:right, :up, :down]        => 8,
-      [:left, :right, :up]        => 9,
-      [:left, :right, :up, :down] => 12
-    }
+  class << self
+    def wall_rules
+      @wall_rules ||= {
+        []                          => 10,
+        [:up]                       => 5,
+        [:down]                     => 10,
+        [:left]                     => 11,
+        [:right]                    => 2,
+        [:right, :up]               => 1,
+        [:right, :down]             => 2,
+        [:left, :right]             => 3,
+        [:left, :down]              => 4,
+        [:up, :down]                => 5,
+        [:left, :up]                => 6,
+        [:left, :up, :down]         => 7,
+        [:right, :up, :down]        => 8,
+        [:left, :right, :up]        => 9,
+        [:left, :right, :up, :down] => 12
+      }.to_a.reverse
+    end
   end
 end
